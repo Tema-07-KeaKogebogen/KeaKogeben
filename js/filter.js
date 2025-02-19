a6a126facc0d7d77908f9d998cb0ffdc698457dc
Cecilie Skafte <cesk0001@stud.kea.dk>
2025-02-19 11:29:30 +0100

    Fixed uniqueCuisines mapping

--- js/filter.js
+++ js/filter.js
@@ -4,43 +4,35 @@ const productListContainer = document.querySelector(".product_list_container");
 
 let allRecipes = []; // Store all recipes for filtering
 
-
 fetch("https://dummyjson.com/recipes?limit=150")
   .then((response) => response.json())
   .then((data) => {
     allRecipes = data.recipes;
-    
+
     // Extract unique cuisines for the filter dropdown
     const uniqueCuisines = ["All", ...new Set(allRecipes.map((recipe) => recipe.cuisine))];
-    
-    // Populate the cuisine dropdown  cuisineSelect.innerHTML = uniqueCuisines
-      .map((cuisine) => `<option value="${cuisine}">${cuisine}</option>`)
-      .join("");
 
-    showList(allRecipes); 
+    // Populate the cuisine dropdown  cuisineSelect.innerHTML = uniqueCuisines
+    uniqueCuisines.map((cuisine) => `<option value="${cuisine}">${cuisine}</option>`).join("");
+
+    showList(allRecipes);
   })
   .catch((error) => console.error("Error fetching recipes:", error));
 
 // Event listener for cuisine filter
 cuisineSelect.addEventListener("change", filterRecipes);
 
-
 function filterRecipes() {
   const selectedCuisine = cuisineSelect.value;
 
-
   let filteredRecipes = allRecipes;
 
- 
   if (selectedCuisine !== "All") {
     filteredRecipes = filteredRecipes.filter((recipe) => recipe.cuisine === selectedCuisine);
   }
 
-
-
   showList(filteredRecipes);
 }
-
 
 function showList(recipes) {
   const markup = recipes
